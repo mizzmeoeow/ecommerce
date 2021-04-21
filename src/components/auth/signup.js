f6900ffe529732fd3663cf3398f7f569f7e8d809
Shanna <shannameow@gmail.com>
2021-04-20 21:22:39 -0400

    built sign in and sign up routes and components

---
+++ src/components/auth/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return <div className="sign-up">sign uppp</div>;
+  }
+}
+
+export default SignUp;
--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
   render() {
-    return <div className="header"></div>;
+    return <div className="navbar"></div>;
   }
 }
 
