ab2173e16a235df92622b66d2cd9273a3c3b28ac
Shanna <shannameow@gmail.com>
2021-04-23 20:05:02 -0400

    finished signup form and grid

--- src/components/auth/signupForm.js
+++ src/components/auth/signupForm.js
@@ -10,21 +10,18 @@ import history from "../../history";
 class SignUpForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
-    const links = [
+    const info = [
       {
         _id: 0,
-        title: "Not registered?  Create account here",
-        onClick: () => history.push("/signup"),
+        title: "At least 6 characters",
       },
       {
         _id: 1,
-        title: "Forgot account email?",
-        onClick: () => console.log("forgot email"),
+        title: "At least one number",
       },
       {
         _id: 2,
-        title: "Forgot password?",
-        onClick: () => console.log("forgot password"),
+        title: "At least one symbol",
       },
     ];
     return (
@@ -54,7 +51,7 @@ class SignUpForm extends Component {
           component={FormInput}
         />
         <Field
-          className="sign-up-form__confirm-password"
+          className="sign-up-form__confirm"
           type="password"
           title="Confirm Password"
           placeholder="Confirm Password"
@@ -65,15 +62,15 @@ class SignUpForm extends Component {
         <div className="sign-up-form__line"></div>
         <Field
           className="sign-up-form__login"
-          onClick={() => console.log("tryna submit")}
+          onClick={() => history.push("/account")}
           type="submit"
-          title="Login"
-          name="Login"
+          title="Create Account"
+          name="Create Account"
           component={FormButton}
         />
         <Field
           className="sign-up-form__back"
-          onClick={() => console.log("tryna go back")}
+          onClick={() => history.push("/signin")}
           type="button"
           title="Back"
           name="Back"
@@ -82,8 +79,8 @@ class SignUpForm extends Component {
         />
         <Details
           className="sign-up-form__details"
-          title="QuickLinks"
-          links={links}
+          title="Password Requirements"
+          info={info}
         />
       </form>
     );
