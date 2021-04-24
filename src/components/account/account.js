4beb5836d7d847ddeb812ae8250f4c1bd4f55c12
Shanna <shannameow@gmail.com>
2021-04-24 19:12:43 -0400

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -8,6 +8,18 @@ import PurchaseHistory from "./purchaseHistory";
 
 class Account extends Component {
   componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: "Shop",
+        path: "/shop",
+      },
+      {
+        _id: 1,
+        title: "Logout",
+        path: "/",
+      },
+    ];
     const navbarLinks = [
       {
         _id: 0,
@@ -23,7 +35,7 @@ class Account extends Component {
       },
     ];
 
-    this.props.setHeaderLinks([]);
+    this.props.setHeaderLinks(headerLinks);
     this.props.setNavbarLinks(navbarLinks);
   }
 
