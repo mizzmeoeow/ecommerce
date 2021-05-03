1c652caa0395241a87ae39403a60dabd906ba1d5
Shanna <shannameow@gmail.com>
2021-05-03 19:58:38 -0400

    review form grid

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 import PageTitle from "../pageTitle";
-import ReviewForm from "./reviewFrom";
+import ReviewForm from "./reviewForm";
 
 class Review extends Component {
   componentDidMount() {
