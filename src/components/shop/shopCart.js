a9c6060cb7bf4cab362ae4d9d963a77f34f81a1f
Shanna <shannameow@gmail.com>
2021-05-04 18:09:45 -0400

    calculating order subtotal and total

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -23,7 +23,10 @@ function CartContent({ className, products }) {
 }
 
 function CartFooter({ className, products }) {
-  const price = 7.96;
+  let subtotal = 0;
+  products.map((cartProduct) => {
+    subtotal += cartProduct.quantity * cartProduct.product.price;
+  });
   return (
     <div className={`${className} cart-footer`}>
       <a
@@ -33,7 +36,7 @@ function CartFooter({ className, products }) {
         Checkout
       </a>
       <div className="cart-footer__subtotal">Subtotal</div>
-      <div className="cart-footer__price">${price}</div>
+      <div className="cart-footer__price">${subtotal}</div>
     </div>
   );
 }
