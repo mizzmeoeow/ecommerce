0660b24d7e59f65d0465ab3516e1ada2cd447bf9
Shanna <shannameow@gmail.com>
2021-05-05 18:29:38 -0400

    built shipping to component

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -6,6 +6,7 @@ import { FormInput, FormButton } from "../formFields";
 
 import history from "../../history";
 import OrderSummary from "./orderSummary";
+import { UnderlinedTitle } from "./infoHelp";
 
 class PaymentForm extends Component {
   render() {
@@ -65,6 +66,16 @@ class PaymentForm extends Component {
           component={FormButton}
         />
         <OrderSummary className="payment-form__order-summary" />
+        <div className="payment-form__shipping-info shipping-info">
+          <UnderlinedTitle
+            className="shipping-info__title"
+            title="Shipping To"
+          />
+          <div className="shipping-info__name small-text">Shanna</div>
+          <div className="shipping-info__address small-text">
+            1234 address goes here
+          </div>
+        </div>
       </form>
     );
   }
